16804e0d433c7b33c1cd69f077620447418908ef
Harry Franzen <harry.franzen@kpn.com>
2015-03-22 17:56:30 +0100

    Boot file aangepast

--- boot.sh
+++ boot.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 #First start the Elasticsearch
-docker run -d --name es hfranzen/elasticsearch 
+docker run -d --name es elasticsearch 
 
 #start the kibana interface
-docker run -d --name kibana --link es:es -p 5601 --name kibana hfranzen/kibana
+docker run -d --name kibana --link es:es -p 5601:5601 --name kibana hfranzen/kibana
 
 #Start logstash interactive and link this container to ElasticSearch
-docker run -i --name logstash --link es:es 
+docker run -i --name logstash --link es:es hfranzen/logstash
 
-#Start a example docker container that sends collectd data to logstash
-docker run -d --link logstash:logstash hfranzen/collectd
+#Start a example docker container that sends collectd data to logstash, thanks to yaronr:
+docker run --name=collectd -v /proc:/proc:ro -e HOSTNAME=collectd --rm --link logstash:logstash yaronr/collectd
