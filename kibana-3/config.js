06632c54546ca92faee731c6bd4a430338ca1cbb
Harry Franzen <harry.franzen@kpn.com>
2015-06-23 11:20:41 +0200

    Kibana 3 met Elasticsearch ontsluiting via NGINX

--- kibana-3/config.js
+++ kibana-3/config.js
@@ -29,7 +29,7 @@ function (Settings) {
      *  +elasticsearch: {server: "http://localhost:9200", withCredentials: true}+
      *
      */
-    elasticsearch: "http://softlayer/elasticsearch/",
+    elasticsearch: "http://softlayer.noudat.nl/elasticsearch/",
 
     /** @scratch /configuration/config.js/5
      *
