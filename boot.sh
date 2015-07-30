21525eb6cadad9f4efc72df8ddb741da2cabc7ba
Harry Franzen <harry.franzen@kpn.com>
2015-07-30 18:13:27 +0200

    Logstash file grok filters added

--- boot.sh
+++ boot.sh
@@ -5,10 +5,10 @@ docker run -d --name es elasticsearch
 
 #start the kibana interface
 docker run -d --name kibana --link es:es -p 5601:5601 --name kibana hfranzen/kibana
-docker run -d --name kibana-3 -p 80:80  --link es:es hfranzen/kibana-3
+#docker run -d --name kibana-3 -p 80:80  --link es:es hfranzen/kibana-3
 
 #Start logstash interactive and link this container to ElasticSearch
 docker run -d --name logstash --link es:es hfranzen/logstash
-docker run -d --name logstash-files -v /data/logs:/logs --link es:es hfranzen/logstash-files
+docker run -d --name logstash-files --privileged=true -v /data/logs:/logs --link es:es hfranzen/logstash-files
 
 docker run -d --name collectd --link logstash:logstash hfranzen/collectd
--- logstash-files/start.sh
+++ logstash-files/start.sh
@@ -1,1 +1,1 @@
-docker run -d --name logstash-files -v /data/logs:/logs --link es:es logstash-files
+docker run -d --name logstash-files --privileged=true -v /data/logs:/logs --link es:es logstash-files
